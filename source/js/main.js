bb1c187aada3ceae7f6d4876341eb62493499c4e
Vitalii Cherenkov <WillardMoto@gmail.com>
2021-08-25 12:05:59 -0400

    add full funcionality trial version

--- source/js/main.js
+++ source/js/main.js
@@ -8,7 +8,7 @@ colors.forEach(item =>{
 })
 
 //creat variable for storage color value
-let currentColor = "";
+let currentColor = '#ffff';
 
 //get background-color value from the clicked element and add it to curretColor variable
 document.getElementById('color-box').onclick = function() {
@@ -17,3 +17,41 @@ document.getElementById('color-box').onclick = function() {
   currentColor = style.backgroundColor;
   console.log(currentColor);
 };
+
+let leftWing = document.getElementById('leftWing');
+let leftWingSmallDot = document.getElementById('leftWingSmallDot');
+let leftWingBigDot = document.getElementById('leftWingBigDot');
+let rightWing = document.getElementById('rightWing');
+let rightWingSmallDot = document.getElementById('rightWingSmallDot');
+let rightWingBigDot = document.getElementById('rightWingBigDot');
+let centerBody = document.getElementById('centerBody');
+let leftAntenna = document.getElementById('leftAntenna');
+let rightAntenna = document.getElementById('rightAntenna');
+
+leftWing.onclick = function() {
+  leftWing.style.fill = currentColor;
+}
+leftWingSmallDot.onclick = function() {
+  leftWingSmallDot.style.fill = currentColor;
+}
+leftWingBigDot.onclick = function() {
+  leftWingBigDot.style.fill = currentColor;
+}
+rightWing.onclick = function() {
+  rightWing.style.fill = currentColor;
+}
+rightWingSmallDot.onclick = function() {
+  rightWingSmallDot.style.fill = currentColor;
+}
+rightWingBigDot.onclick = function() {
+  rightWingBigDot.style.fill = currentColor;
+}
+centerBody.onclick = function() {
+  centerBody.style.fill = currentColor;
+}
+leftAntenna.onclick = function() {
+  leftAntenna.style.fill = currentColor;
+}
+rightAntenna.onclick = function() {
+  rightAntenna.style.fill = currentColor;
+}
