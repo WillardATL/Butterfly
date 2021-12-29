1907ccaa148041f379b9a9b2c0431aca79da24ca
Vitalii Cherenkov <WillardMoto@gmail.com>
2021-12-29 18:05:26 -0500

    simplified code, add heading, add colors

--- build/js/main.js
+++ build/js/main.js
@@ -1,57 +1,22 @@
-//add and remove active class to the clicked element
+//creat variable for color value
+let currentColor = '#ff6666';
+
+//choose the current color for coloring
 const colors = document.querySelectorAll('.color__box');
-colors.forEach(item =>{ 
+colors.forEach(item => { 
   item.addEventListener('click', (e) =>{
   colors.forEach(el=>{ el.removeAttribute('id'); });
-  item.setAttribute('id', 'color-active')
+  item.setAttribute('id', 'color-active');
+  currentColor = getComputedStyle(item).backgroundColor
   });
 })
 
-//creat variable for storage color value
-let currentColor = '#ffff';
 
-//get background-color value from the clicked element and add it to curretColor variable
-document.getElementById('color-box').onclick = function() {
-  let colorActive = document.getElementById('color-active');
-  let style = getComputedStyle(colorActive);
-  currentColor = style.backgroundColor;
-  console.log(currentColor);
-};
-
-let leftWing = document.getElementById('leftWing');
-let leftWingSmallDot = document.getElementById('leftWingSmallDot');
-let leftWingBigDot = document.getElementById('leftWingBigDot');
-let rightWing = document.getElementById('rightWing');
-let rightWingSmallDot = document.getElementById('rightWingSmallDot');
-let rightWingBigDot = document.getElementById('rightWingBigDot');
-let centerBody = document.getElementById('centerBody');
-let leftAntenna = document.getElementById('leftAntenna');
-let rightAntenna = document.getElementById('rightAntenna');
-
-leftWing.onclick = function() {
-  leftWing.style.fill = currentColor;
-}
-leftWingSmallDot.onclick = function() {
-  leftWingSmallDot.style.fill = currentColor;
-}
-leftWingBigDot.onclick = function() {
-  leftWingBigDot.style.fill = currentColor;
-}
-rightWing.onclick = function() {
-  rightWing.style.fill = currentColor;
-}
-rightWingSmallDot.onclick = function() {
-  rightWingSmallDot.style.fill = currentColor;
-}
-rightWingBigDot.onclick = function() {
-  rightWingBigDot.style.fill = currentColor;
-}
-centerBody.onclick = function() {
-  centerBody.style.fill = currentColor;
-}
-leftAntenna.onclick = function() {
-  leftAntenna.style.fill = currentColor;
-}
-rightAntenna.onclick = function() {
-  rightAntenna.style.fill = currentColor;
-}
+//assign a color to the element by clicking
+const areaForColoring = document.querySelectorAll('.areaForColoring');
+console.log(areaForColoring);
+areaForColoring.forEach( (element) => {
+  element.addEventListener('click', (e) =>{
+    element.style.fill = currentColor;
+    });
+  })
